4c5536a19513b236bfa33ff5fb3a8275d1530410
Olav <olav.eikanger@uib.no>
2026-04-11 11:29:24 +0200

    Now serves an html page with canvas

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,13 @@
 import express from "express"
+import path from "path"
 
 const app = express();
 const port = 3000
 
-app.use(express.static("./public"))
+app.use(express.static(path.join(import.meta.dirname, "public")))
 
 app.get("/", (req, res) => {
-    res.send("Hello StudyStarPlanet!")
-    //res.status(200).render("pages/index.html")
+    res.status(200).sendFile("./index.html", {root: path.join(import.meta.dirname, "pages")})
 })
 
 app.listen(port, () => {
--- src/public/scripts/index.ts
+++ src/public/scripts/index.ts
@@ -3,3 +3,5 @@ const ctx = canvas.getContext("2d") as CanvasRenderingContext2D
 
 ctx.fillStyle = "blue"
 ctx.fillRect(10, 10, 150, 100)
+
+console.log("Great success")
